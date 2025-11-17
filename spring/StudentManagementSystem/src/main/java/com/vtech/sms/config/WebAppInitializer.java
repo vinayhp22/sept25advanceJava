57419d1eabe5f7194c3284896b00552958573c30
vinayhp22 <113164298+vinayhp22@users.noreply.github.com>
2025-11-17 15:57:02 +0530

    Spring MVC - 1

---
+++ spring/StudentManagementSystem/src/main/java/com/vtech/sms/config/WebAppInitializer.java
@@ -0,0 +1,30 @@
+package com.vtech.sms.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		System.out.println("getRootConfigClasses...");
+		return new Class<?>[]{ AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		System.out.println("getServletConfigClasses...");
+		return new Class<?>[]{ WebMvcConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		System.out.println("getServletMappings");
+		return new String[]{"/"};
+	}
+	static {
+	    System.out.println("WebAppInitializer LOADED !!!");
+	}
+
+}
---
+++ spring/StudentManagementSystem/src/main/java/com/vtech/sms/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.vtech.sms.service;
+
+import java.util.List;
+
+import com.vtech.sms.enitity.Student;
+
+public interface StudentService {
+
+void saveStudent(Student student);
+	
+	Student getStudent(int id);
+	
+	void updateStudent(Student student);
+	
+	List<Student> getAllStudents();
+	
+	void deleteStudent(int id);
+}
